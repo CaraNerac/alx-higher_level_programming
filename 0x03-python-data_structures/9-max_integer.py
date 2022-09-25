e439133567ff8d79c857e294a2cc44eb63bdcd70
CaraNerac <carenjambi@gmail.com>
2022-09-25 16:48:07 +0000

    03 python

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        my_list.sort()
+        return (my_list.pop())
+    else:
+        return (None)
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        my_list.sort()
+        return (my_list.pop())
+    else:
+        return (None)
